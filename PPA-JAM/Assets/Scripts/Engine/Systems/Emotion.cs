5ecb9de77a57d5c577860efb70b8573ae9e5d3fc
Jesus Alberto del Moral Cupil <idv18c.jmoral@uartesdigitales.edu.mx>
2020-05-27 14:40:08 -0500

    BrackEnd - Worker & Emotions
    Update List:
    > The Emotion class was created
    > The Worker class was created
    > Emotion setters and getters were implemented

---
+++ PPA-JAM/Assets/Scripts/Engine/Systems/Emotion.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public enum basicEmotions {
+    Happy,
+    Sad,
+    Angry,
+    Fear,
+    Trust,
+    Aversion,
+    Anticipation
+          
+};
+
+public enum complexEmotions { 
+    Frustrated,
+    Stressed,
+    Anxiety,
+    Guilt,
+    Jealousy,
+    Shame,
+    Optimism,
+    Scare,
+    Disappointment,
+    Remorse,
+    Contempt
+};
+
+
+public class Emotion : MonoBehaviour {
+
+
+    private basicEmotions m_BasicEmotions;
+    private complexEmotions m_ComplexEmotions;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void init() {
+
+    }
+
+    public basicEmotions getBasicEmotions() {
+        return m_BasicEmotions;
+    }
+    
+    public complexEmotions getComplexEmotions() {
+        return m_ComplexEmotions;
+    }
+
+    public void setBasicEmotions(basicEmotions _basicEmotion) {
+        m_BasicEmotions = _basicEmotion;
+    }
+   
+    public void setComplexEmotions(complexEmotions _complexEmotions) {
+        m_ComplexEmotions = _complexEmotions;
+    }
+}
